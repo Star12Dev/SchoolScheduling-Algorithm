38df775da2804fda1319419f8b4061c51da22ff4
Star12Dev <gold12starm@gmail.com>
2019-03-25 12:36:39 -0400

    section function

--- ClientApp/src/app/section/section.component.ts
+++ ClientApp/src/app/section/section.component.ts
@@ -142,6 +142,59 @@ export class SectionComponent implements OnInit {
     }
   }
 
+if (scheduleData && scheduleData.length > 0) {
+      for (let i = 0; i < scheduleData.length; i++) {
+        let eObj = scheduleData[i];
+        if (this.schoolArray.indexOf(eObj['school_name']) === -1) {
+          this.schoolArray.push(eObj['school_name']);
+        }
+
+        if (this.studentArray.indexOf(eObj['student_number']) === -1) {
+          this.studentArray.push(eObj['student_number']);
+        }
+
+        if (this.semesterArray.indexOf(eObj['semester']) === -1) {
+          this.semesterArray.push(eObj['semester']);
+        }
+
+        if (this.termArray.indexOf(eObj['term']) === -1) {
+          this.termArray.push(eObj['term']);
+        }
+
+        if (this.courseSectionArray.indexOf(eObj['course_section']) === -1) {
+          this.courseSectionArray.push(eObj['course_section']);
+        }
+
+        if (this.courseGradeArray.indexOf(eObj['course_grade']) === -1) {
+          this.courseGradeArray.push(eObj['course_grade']);
+        }
+
+        if (this.roomNameArray.indexOf(eObj['room_name']) === -1) {
+          this.roomNameArray.push(eObj['room_name']);
+        }
+
+        if (this.roomNumberArray.indexOf(eObj['room_number']) === -1) {
+          this.roomNumberArray.push(eObj['room_number']);
+        }
+
+        if (this.teacherArray.indexOf(eObj['teacher_name']) === -1) {
+          this.teacherArray.push(eObj['teacher_name']);
+        }
+
+        if (this.periodArray.indexOf(eObj['period']) === -1) {
+          this.periodArray.push(eObj['period']);
+        }
+      }
+
+      this.semesterArray.sort();
+      this.termArray.sort();
+      this.periodArray.sort();
+      this.roomNameArray.sort();
+
+      this.updateContent();
+    }
+  }
+
   changeTeacher($event) {
     this.updateContent();
   }
