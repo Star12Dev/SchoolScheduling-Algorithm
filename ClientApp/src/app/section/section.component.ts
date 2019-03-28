7c8fd9b01ef66521de8c1b067d96732cc048713e
Star12Dev <gold12starm@gmail.com>
2019-03-28 10:26:12 -0400

    rood edit

--- ClientApp/src/app/section/section.component.ts
+++ ClientApp/src/app/section/section.component.ts
@@ -142,59 +142,6 @@ export class SectionComponent implements OnInit {
     }
   }
 
-if (scheduleData && scheduleData.length > 0) {
-      for (let i = 0; i < scheduleData.length; i++) {
-        let eObj = scheduleData[i];
-        if (this.schoolArray.indexOf(eObj['school_name']) === -1) {
-          this.schoolArray.push(eObj['school_name']);
-        }
-
-        if (this.studentArray.indexOf(eObj['student_number']) === -1) {
-          this.studentArray.push(eObj['student_number']);
-        }
-
-        if (this.semesterArray.indexOf(eObj['semester']) === -1) {
-          this.semesterArray.push(eObj['semester']);
-        }
-
-        if (this.termArray.indexOf(eObj['term']) === -1) {
-          this.termArray.push(eObj['term']);
-        }
-
-        if (this.courseSectionArray.indexOf(eObj['course_section']) === -1) {
-          this.courseSectionArray.push(eObj['course_section']);
-        }
-
-        if (this.courseGradeArray.indexOf(eObj['course_grade']) === -1) {
-          this.courseGradeArray.push(eObj['course_grade']);
-        }
-
-        if (this.roomNameArray.indexOf(eObj['room_name']) === -1) {
-          this.roomNameArray.push(eObj['room_name']);
-        }
-
-        if (this.roomNumberArray.indexOf(eObj['room_number']) === -1) {
-          this.roomNumberArray.push(eObj['room_number']);
-        }
-
-        if (this.teacherArray.indexOf(eObj['teacher_name']) === -1) {
-          this.teacherArray.push(eObj['teacher_name']);
-        }
-
-        if (this.periodArray.indexOf(eObj['period']) === -1) {
-          this.periodArray.push(eObj['period']);
-        }
-      }
-
-      this.semesterArray.sort();
-      this.termArray.sort();
-      this.periodArray.sort();
-      this.roomNameArray.sort();
-
-      this.updateContent();
-    }
-  }
-
   changeTeacher($event) {
     this.updateContent();
   }
@@ -289,41 +236,3 @@ if (scheduleData && scheduleData.length > 0) {
     this.gridCSS = 'repeat(auto-fit, minmax(' + min + '%,' + max + '%))';
   }
 }
-
-    this.sectionObj[0] = [];
-
-    for (let i = 0; i < this.semesterArray.length; i++) {
-      for (let j = 0; j < this.periodArray.length; j++) {
-        let key = 'S' + (i + 1).toString() + 'P' + (j + 1).toString();
-        this.head.push(key);
-        this.head1.push('1234567890');
-        this.sectionObj[0].push(key);
-        hashPeriodSemester[key] = i * this.periodArray.length + j;
-      }
-    }
-
-    this.totalAnalysis = this.head.length;
-
-    for (let i = 0; i < this.semesterArray.length; i++) {
-      semesterHash[this.semesterArray[i]] = i;
-    }
-
-    for (let i = 0; i < this.periodArray.length; i++) {
-      periodHash[this.periodArray[i]] = i;
-    }
-
-    this.maxColumn = this.semesterArray.length * this.periodArray.length + 1;
-
-    for (let each in tempData) {
-      let split = each.split('|||');
-      let obj = tempData[each];
-      let indS = semesterHash[split[3]] + 1;
-      let indP = periodHash[split[4]] + 1;
-      let indSPKey = 'S' + indS.toString() + 'P' + indP.toString();
-      let indSP = hashPeriodSemester[indSPKey];
-    }
-
-    let min = Math.floor(100 / this.maxColumn);
-    let max = Math.ceil(100 / this.maxColumn);
-    this.gridCSS = 'repeat(auto-fit, minmax(' + min + '%,' + max + '%))';
-  }
